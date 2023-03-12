f81055d2dafe7cbcb33c0c84f581b8353754c3ac
your-username <your-email@gmail.com>
2023-03-12 14:46:47 +0900

    Finished

---
+++ ShirGudOr 2/app/Models/Brand.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Brand extends Model
+{
+    use HasFactory;
+
+    protected $table = 'brands';
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'category_id',
+        'status'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
+}
